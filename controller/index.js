bc68047f4e348b24f3eb2c66cd09eab228931696
ehroy <68073896+ehroy@users.noreply.github.com>
2024-10-19 05:40:46 +0000

    update controller

--- controller/index.js
+++ controller/index.js
@@ -4,11 +4,11 @@ const getBalance = expressAsyncHandler(async (req, res) => {
     throw new Error("please add field");
   }
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+  const apikey = authHeader && authHeader.split(" ")[1]; // Bearer <token>
 
-  if (!token) return res.status(401).json(new Error("no auth provide"));
+  if (!apikey) return res.status(401).json(new Error("no auth provide"));
   try {
-    const apiUrl = `https://smshub.org/stubs/handler_api.php?api_key=${token}&action=getBalance&currency=USD`;
+    const apiUrl = `https://smshub.org/stubs/handler_api.php?api_key=${apikey}&action=getBalance`;
     const response = await fetch(apiUrl, {
       headers: {
         "Content-Type": "application/json",
@@ -22,6 +22,43 @@ const getBalance = expressAsyncHandler(async (req, res) => {
 
     const respone = await response.text();
     console.log(respone);
+    const data = {
+      status: true,
+      message: {
+        balance: respone.split(":")[1],
+        action: respone.split(":")[0],
+      },
+    };
+    res.status(200).json(data);
+  } catch (error) {
+    throw new Error("error fetching data from api");
+  }
+});
+const listFound = expressAsyncHandler(async (req, res) => {
+  if (!req.body) {
+    throw new Error("please add field");
+  }
+  const authHeader = req.headers["authorization"];
+  const apikey = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+
+  if (!apikey) return res.status(401).json(new Error("no auth provide"));
+  try {
+    const {  country } = req.body;
+    const apiUrl = `https://smshub.org/stubs/handler_api.php?api_key=${apikey}&action=getNumbersStatus&country=${country}`;
+    console.log(apiUrl)
+    const response = await fetch(apiUrl, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // Periksa jika response dari API eksternal berhasil
+    if (!response.ok) {
+      throw new Error(`API Error: ${response.statusText}`);
+    }
+
+    const respone = await response.text();
+    console.log(respone)
     const data = {
       status: true,
       message: {
@@ -39,9 +76,14 @@ const orderNumber = expressAsyncHandler(async (req, res) => {
   if (!req.body) {
     throw new Error("please add field");
   }
+  const authHeader = req.headers["authorization"];
+  const apikey = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+
+  if (!apikey) return res.status(401).json(new Error("no auth provide"));
   try {
-    const { apikey, country, operator, service } = req.body;
+    const {  country, operator, service } = req.body;
     const apiUrl = `https://smshub.org/stubs/handler_api.php?api_key=${apikey}&action=getNumber&service=${service}&operator=${operator}&country=${country}`;
+    console.log(apiUrl)
     const response = await fetch(apiUrl, {
       headers: {
         "Content-Type": "application/json",
@@ -54,6 +96,7 @@ const orderNumber = expressAsyncHandler(async (req, res) => {
     }
 
     const respone = await response.text();
+
     const data = {
       status: true,
       message: {
@@ -68,4 +111,96 @@ const orderNumber = expressAsyncHandler(async (req, res) => {
   }
 });
 
-export { orderNumber, getBalance };
+const actionNumber = expressAsyncHandler(async (req, res) => {
+  if (!req.body) {
+    throw new Error("please add field");
+  }
+  const authHeader = req.headers["authorization"];
+  const apikey = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+
+  if (!apikey) return res.status(401).json(new Error("no auth provide"));
+  try {
+    const {  status, id } = req.body;
+    const apiUrl = `https://smshub.org/stubs/handler_api.php?api_key=${apikey}&action=setStatus&status=${status}&id=${id}`;
+    console.log(apiUrl)
+    const response = await fetch(apiUrl, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // Periksa jika response dari API eksternal berhasil
+    if (!response.ok) {
+      throw new Error(`API Error: ${response.statusText}`);
+    }
+
+    const respone = await response.text();
+    if(respone === 'BAD_STATUS'){
+      const data = {
+        status: false,
+        message: respone,
+      };
+      res.status(403).json(data);
+    }else{
+      const data = {
+        status: true,
+        message: {
+          id: id,
+          number: status,
+          action: respone,
+        },
+      };
+      res.status(200).json(data);
+    }
+  } catch (error) {
+    throw new Error("error fetching data from api");
+  }
+});
+
+const actionNumber = expressAsyncHandler(async (req, res) => {
+  if (!req.body) {
+    throw new Error("please add field");
+  }
+  const authHeader = req.headers["authorization"];
+  const apikey = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+
+  if (!apikey) return res.status(401).json(new Error("no auth provide"));
+  try {
+    const {  status, id } = req.body;
+    const apiUrl = `https://smshub.org/stubs/handler_api.php?api_key=${apikey}&action=setStatus&status=${status}&id=${id}`;
+    console.log(apiUrl)
+    const response = await fetch(apiUrl, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // Periksa jika response dari API eksternal berhasil
+    if (!response.ok) {
+      throw new Error(`API Error: ${response.statusText}`);
+    }
+
+    const respone = await response.text();
+    if(respone === 'BAD_STATUS'){
+      const data = {
+        status: false,
+        message: respone,
+      };
+      res.status(403).json(data);
+    }else{
+      const data = {
+        status: true,
+        message: {
+          id: id,
+          number: status,
+          action: respone,
+        },
+      };
+      res.status(200).json(data);
+    }
+  } catch (error) {
+    throw new Error("error fetching data from api");
+  }
+});
+
+export { orderNumber, getBalance,listFound,actionNumber };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { orderNumber, getBalance } from "../controller/index.js";
+import { orderNumber, getBalance,listFound,actionNumber } from "../controller/index.js";
 const router = express.Router();
 
 router.route("/order").post(orderNumber);
+router.route("/action").post(actionNumber);
 router.route("/balance").get(getBalance);
+router.route("/list").get(listFound);
+
+
 
 export default router;
